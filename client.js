d5b0e6d40bd856dbac84958599a7331a53239ce9
Erika-mimi <erikayoshida5@gmail.com>
2021-12-02 07:14:25 +0000

    Added name and move up

--- client.js
+++ client.js
@@ -4,17 +4,25 @@ const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541,
-
-
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+//サーバーに接続されたら下記のactionが行われる
   conn.on("connect", () => {
     console.log("Successfully connected");
+    conn.write('Name: ERK');
   })
+  //サーバーから情報を受け取る
+  conn.on('data', (data) => {
+    console.log('Server says: ', data)
+  });
+//自由に動かせるようになる
+  process.stdin.on('data', function(message) {
+    conn.write(message);
+    conn.write('Move: up');
 
+  });
   return conn;
 };
 
